5aaddda1d3e53e99809f7110dccd5a16a4ba39f3
Scott Morrison <scott@tqft.net>
2012-06-06 22:53:43 -0500

    core.m test works under sbt test too; should learn how to find resources properly, and to include omath sources in the build

--- core/src/main/scala/org/omath/core/Core.scala
+++ core/src/main/scala/org/omath/core/Core.scala
@@ -5,30 +5,37 @@ import org.omath.SymbolExpression
 import org.omath.parser.SyntaxParser
 import org.omath.kernel.Kernel
 import scala.io.Source
+import scala.collection.JavaConversions
 
 sealed trait Result[T]
 case class Success[T](value: T) extends Result[T]
 case class Failure[T](message: String) extends Result[T]
 
 trait Core extends Kernel {
-	def evaluate(syntax: String): Result[Expression] = {
-	  SyntaxParser(syntax)(symbolizer) match {
-	    case Left(expression)  => Success(evaluate(expression))
-	    case Right(message) => Failure(message)
-	  }
-	}
-	def slurp(code: Source) {
-	  for(line <- code.getLines) {
-	    evaluate(line) match {
-	      case Success(_) => 
-	      case Failure(message) => System.err.println(message)
-	    }
-	  }
-	}
-	
-	slurp(Source.fromFile("../core/src/main/omath/core.m"))
-	
-	// FIXME look at $Context and $ContextPath
-	private def symbolizer = { s: String => SymbolExpression(s, "System") }
-	
+  def evaluate(syntax: String): Result[Expression] = {
+    SyntaxParser(syntax)(symbolizer) match {
+      case Left(expression) => Success(evaluate(expression))
+      case Right(message) => Failure(message)
+    }
+  }
+  def slurp(code: Source) {
+    for (line <- code.getLines) {
+      evaluate(line) match {
+        case Success(_) =>
+        case Failure(message) => System.err.println(message)
+      }
+    }
+  }
+
+  {
+    import JavaConversions._
+    val resources: Iterator[java.net.URL] = this.getClass.getClassLoader.getResources("")
+    val coreClasses = resources.map(_.toString).filter(_.contains("/core/")).next
+    val `core.m` = coreClasses.take(coreClasses.indexOf("/core/") + 6) + "src/main/omath/core.m"
+    slurp(Source.fromURI(new java.net.URI(`core.m`)))
+  }
+
+  // FIXME look at $Context and $ContextPath
+  private def symbolizer = { s: String => SymbolExpression(s, "System") }
+
 }
