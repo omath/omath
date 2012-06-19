c6a39aea43dc499748fd5481f6fe1c91696f9a6d
Scott Morrison <scott@tqft.net>
2012-06-19 01:13:38 -0700

    trying heroku again

--- ui/src/main/scala-2.9/org/omath/ui/rest/Web.scala
+++ ui/src/main/scala-2.9/org/omath/ui/rest/Web.scala
@@ -5,6 +5,7 @@ import org.mortbay.jetty.Server
 import org.mortbay.jetty.webapp.WebAppContext
 import org.mortbay.jetty.nio._
 import scala.util.Properties
+import java.io.File
 
 object Web extends App {
   val server = new Server
@@ -15,7 +16,8 @@ object Web extends App {
   val context = new WebAppContext()
   context.setServer(server)
   context.setContextPath("/")
-  context.setWar("src/main/webapp")
+  val warLocation = Seq("src/main/webapp/", "ui/src/main/webapp").find(p => new File(p).exists).get
+  context.setWar(warLocation)
 
   server.addHandler(context)
 
