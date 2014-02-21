a683fd17a6aec4e2252b72769ff4f981e26c6154
Scott Morrison <scott@tqft.net>
2014-02-21 20:16:43 +0100

    ripping out bowler; finagle seems to achieve the same in a few lines

--- project/omath.scala
+++ project/omath.scala
@@ -44,7 +44,7 @@ object Omath extends Build {
 
     lazy val rest = Project(id = "omath-ui-rest",
 				base = file("ui/rest"),
-				settings = buildSettings ++ bowlerWebapp ++ heroku) dependsOn(tungstenCore)
+				settings = buildSettings ++ heroku ++ Seq(libraryDependencies ++= Seq(finagle.core, finagle.http))) dependsOn(tungstenCore)
 
     lazy val webstart = Project(id = "omath-ui-webstart",
 				base = file("ui/webstart"),
@@ -70,31 +70,17 @@ object BuildSettings {
 
   val buildOrganization = "org.omath"
   val buildVersion      = "0.0.1-SNAPSHOT"
-  val buildScalaVersion = "2.10.3"
-  val buildCrossScalaVersions = Seq("2.9.2", "2.10.3")
+  val buildScalaVersion = "2.11.0-M8"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     version      := buildVersion,
     scalaVersion := buildScalaVersion,
-    crossScalaVersions := buildCrossScalaVersions,
-//    scalacOptions += "-feature",
+    scalacOptions += "-feature",
     scalacOptions += "-unchecked",
-//    scalacOptions += "-Xprint:typer",
     publishTo    := Some(Resolver.sftp("toolkit.tqft.net Maven repository", "tqft.net", "tqft.net/releases") as ("scottmorrison", new java.io.File("/Users/scott/.ssh/id_rsa"))),
     resolvers    := sonatypeResolvers ++ tqftResolvers,
-    libraryDependencies <+= (scalaVersion) { sv =>
-        val (scalatestVersion, scalatestScalaVersion) = sv match {
-                case sv if sv.startsWith("2.9.") => ("1.8", "2.9.0")
-                case sv if sv.startsWith("2.10.") => ("1.8-SNAPSHOT", "2.10.0-M3")
-        }
-        "org.scalatest" % ("scalatest_" + scalatestScalaVersion) % scalatestVersion % "test"
-    },
-    unmanagedSourceDirectories in Compile <+= (baseDirectory, scalaVersion)((bd, sv) => sv match {
-      case sv if sv startsWith "2.9." => bd / "src" / "main" / "scala-2.9"
-      case sv if sv startsWith "2.10." => bd / "src" / "main" / "scala-2.10"
-    }),
-    libraryDependencies ++= Seq(junit, slf4j),
+    libraryDependencies ++= Seq(junit, slf4j, scalatest),
     exportJars := true,
     unmanagedResourceDirectories in Compile <+= (baseDirectory) { bd => bd / "src" / "main" / "omath" },
     StartScriptPlugin.stage in Compile := Unit, 			// fake 'stage' task in the aggregate project, per https://github.com/typesafehub/xsbt-start-script-plugin
@@ -128,15 +114,11 @@ object Dependencies {
 	val junit = "junit" % "junit" % "4.8" % "test"
 	val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.6.1"
         val apfloat = "org.apfloat" % "apfloat" % "1.6.3"               // arbitrary precision integers and floats; much better than BigInt and BigDecimal
-	val bowler = "org.bowlerframework" % "core_2.9.1" % "0.6"
-	val jetty = "org.mortbay.jetty" % "jetty" % "6.1.26" % "container, compile"
-	val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-	val includeBowlerIn29 = libraryDependencies <++= (scalaVersion) {
-	      	case sv if sv startsWith "2.9." => Seq(bowler, jetty, servlet)
-      		case sv if sv startsWith "2.10." => Seq(jetty, servlet)
-	}
-	val bowlerWebapp = Seq(includeBowlerIn29) ++ com.github.siasia.WebPlugin.webSettings
 	val jline = "jline" % "jline" % "1.0"
+	object finagle {
+		val core = "com.twitter" %% "finagle-core" % "6.1.0"
+		val http = "com.twitter" %% "finagle-http" % "6.1.0"
+	}
 	object commons {
 		val codec = "commons-codec" % "commons-codec" % "1.6"
 	}
